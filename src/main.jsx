8e7a50a71f0510207c9a560ee5ae2c2b4835ceb2
Deepak7200 <mail.deepaksehrawat@gmail.com>
2026-04-02 16:25:56 +0530

    Added react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 
 // Library imports
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 
 // Component imports
@@ -11,5 +12,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <App />
+        <Toaster/>
     </BrowserRouter>
 )
