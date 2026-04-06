522a2a530aecb0de610a9837a49de66c6f3abfd1
Deepak7200 <mail.deepaksehrawat@gmail.com>
2026-04-06 14:49:53 +0530

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Route,Routes } from 'react-router-dom'
 
 import AboutUs from './Pages/AboutUs'
+import Contact from './Pages/Contact'
 import Courses from './Pages/Course/CourseList'
 import HomePage from './Pages/HomePage'
 import Login from './Pages/Login'
@@ -18,6 +19,8 @@ function App() {
          <Route path="/" element={<HomePage/>}> Home </Route>
          <Route path="/about" element={<AboutUs/>}> About Us </Route>
          <Route path="/courses" element={<Courses/>}> Courses </Route> 
+         <Route path="/contact" element={<Contact/>}> Courses </Route> 
+
          <Route path="/signup" element={<Signup/>}> Sign Up </Route>
          <Route path="/login" element={<Login/>}> Login </Route>
          <Route path="*" element={<NotFound/>}> No Page Found </Route>
